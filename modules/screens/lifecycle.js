0f376ec61ef89cbf58117282889964600901ccd9
umairyaqub31 <umairyaqub31@gmail.com>
2020-08-22 15:11:03 +0500

    image commit

--- modules/screens/lifecycle.js
+++ modules/screens/lifecycle.js
@@ -1,5 +1,6 @@
 import React from'react';
-import {View, Button, Text} from 'react-native';
+import {View, Button, Text, StyleSheet,Image,TouchableOpacity} from 'react-native';
+
 
 
 
@@ -7,18 +8,12 @@ export default class Lifecycle extends React.Component{
              constructor(){
              super();
              this.state={
-            count: ''
+             count: '',
+             source: require('./../../assets/images/second.jpg')
                      
         }
     }
-    
-
-         //saySomething = (something) =>{            
-            /*console.log(something)
-             }*/
-            
-
-         
+                
          copmonentDidMount()
          {
            this.setState();
@@ -39,20 +34,29 @@ export default class Lifecycle extends React.Component{
          }
 
          
-        handleClick = () => {
+        /*Addition = () => {
             this.setState({
-             
+                count: this.state.count + 1
             });
         }
 
-        mius = () => {
+        minus = () => {
             this.setState({
-
+                count: this.state.count - 1
             });
         }
+        */
+        changeImage = () => {
+             this.setState({
+                 source: require('./../../assets/images/iot.jpg')
+                 
+             });
+         }
 
     render(){
-      
+             
+                  
+
         return(
             <View>
                <Text> LifeCycle Methods</Text>
@@ -63,15 +67,54 @@ export default class Lifecycle extends React.Component{
                </Button>
 
                <Button 
+                   style={styles.btn} 
                    title="Subtract"
                    onPress={()=>this.setState({count: this.state.count - 1})}>
                </Button>
 
 
+           <View style={styles.btn}>
+                <TouchableOpacity  onPress={()=>this.changeImage}>               
+                <Text>{this.state.source}</Text>
+                <Image
+                          style={styles.img}
+                          source = {this.state.source}                                                                
+                />
+                </TouchableOpacity>  
+
 
                
+                <Button
+                          style={styles.btn1}
+                          title='Press Me'>
+                          onPress={this.changeImage}  
+                </Button>    
+                </View>
+                         
+              
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+                 
+                 btn: {
+                     marginTop: 50,
+                     justifyContent:'center',
+                 },
+                 
+                 img: {
+                     justifyContent: 'center'
+                 },
+
+                 btn1: {
+                    borderRadius: 20,
+                    marginLeft:50, 
+                    marginTop:20,
+                    alignItems:'center',
+                    justifyContent: 'center'                
+
+                 }
+});
 
